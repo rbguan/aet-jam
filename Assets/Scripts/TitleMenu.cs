391de3146352c1bf1b145bb10663d64e8f7c77ef
Ali Kedwaii <ali.kedwaii@utexas.edu>
2021-01-10 19:45:03 -0600

    Title menu stuff (ugly tho)

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    public GameObject credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        credits.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (credits.activeSelf)
+                credits.SetActive(false);
+        }
+    }
+
+    public void OnPlay()
+    {
+        SceneManager.LoadScene("PaperDialogScene");
+    }
+
+    public void OnCredits()
+    {
+        credits.SetActive(true);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }    
+}
