b44eb38364a68354ef63119c3ffc8d80d2c1485c
Ali Kedwaii <ali.kedwaii@utexas.edu>
2021-01-10 19:23:02 -0600

    Adding pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject panel;
+    public CharacterInteract characterInteract;
+
+    private bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Time.timeScale = 0f;
+                panel.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+                characterInteract.isPaused = true;
+                isPaused = true;
+            }
+            else
+            {
+                OnResume();
+            }
+        }
+    }
+
+    public void OnResume()
+    {
+        panel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+        characterInteract.isPaused = false;
+        isPaused = false;
+    }
+
+    public void OnRetry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void OnReturn()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("TitleScene");
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+}
